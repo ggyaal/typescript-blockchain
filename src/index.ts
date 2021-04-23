625d22daa29edae18dc8bff276f671c8f59037fb
ggyaal <72740806+ggyaal@users.noreply.github.com>
2021-04-24 00:56:40 +0900

    update 1

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+import * as CryptoJS from "crypto-js";
+
 class Block {
   public index: number;
   public hash: string;
   public previousHash: string;
   public data: string;
   public timestemp: number;
+
+  static calculateBlockHash = (
+    index: number,
+    previousHash: string,
+    timestemp: number,
+    data: string
+  ): string =>
+    CryptoJS.SHA256(index + previousHash + timestemp + data).toString();
+
   constructor(
     index: number,
     hash: string,
@@ -27,7 +38,13 @@ const firstBlock = new Block(
   1226
 );
 
-let blockChain: [Block] = [firstBlock];
+let blockChain: Block[] = [firstBlock];
+
+const getBlockChain = (): Block[] => blockChain;
+
+const getLatestBlock = (): Block => blockChain[blockChain.length - 1];
+
+const getNewTimeStemp = (): number => Math.round(new Date().getTime() / 1000);
 
 console.log(blockChain);
 
